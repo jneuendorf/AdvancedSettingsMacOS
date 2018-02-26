1b8b6cd0a69adb799add490e7136dbb4ffc988eb
Jim Neuendorf <jim.neuendorf@gmx.de>
2018-02-26 20:39:37 +0100

    enhanced UI, command creation works

--- options/static/options/index.js
+++ options/static/options/index.js
@@ -1,4 +1,42 @@
-import commands from './commands.js'
+import commandConfig from './commands.js'
 
 
-console.log(commands)
+$(".send-command").click(event => {
+    const button = $(event.target)
+    button.addClass('is-loading')
+
+    const value_source = button.parent().find('[data-value-source]')
+    const section_name = button.attr('data-section-name')
+    const command_id = button.attr('data-command-id')
+
+    let value
+    if (value_source.attr('type') === 'checkbox') {
+        value = value_source.is(':checked')
+    }
+    else {
+        value = value_source.val()
+    }
+
+    fetch('api/', {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        method: 'POST',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        body: JSON.stringify({
+            section_name,
+            command_id,
+            input: value,
+        })
+    })
+    .then(response => response.json())
+    .then(json => {
+
+        button.removeClass('is-loading')
+    })
+    .catch(error => {
+        console.error(error)
+        button.removeClass('is-loading')
+    })
+})
--- options/views.py
+++ options/views.py
@@ -1,7 +1,33 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+import json
 
-# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+
+from .command_config import command_config
+
 
 def index(request):
-    return render(request, 'options/index.html')
+    return render(
+        request,
+        'options/index.html',
+        {
+            'command_config': command_config,
+        },
+    )
+
+
+@csrf_exempt
+def api(request, *args, **kwargs):
+    data = json.loads(request.body)
+    section_name = data['section_name']
+    command_id = data['command_id']
+    input = data['input']
+    # if request.POST
+    command_data = command_config[section_name][command_id]
+    command_template = command_data['command']
+    input = command_data['stringify_input'](command_data['parse_input'](input))
+
+    print('would run:')
+    print(command_template.format(input))
+    return HttpResponse('[]')
