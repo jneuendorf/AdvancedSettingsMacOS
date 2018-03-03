5ca834d30ee2d0d6d878080abd888275d419f8d7
Jim Neuendorf <jim.neuendorf@gmx.de>
2018-03-03 13:39:46 +0100

    added mail commands

--- options/command_config.py
+++ options/command_config.py
@@ -909,11 +909,36 @@ raw_command_config = {
         },
     },
     'Mail': {
-        # '': {
-        #     'label': '',
-        #     'command': '',
-        #     'type': '',
-        # },
+        'DisableReplyAnimations': {
+            'label': 'Disable send and reply animations.',
+            'command': 'defaults write com.apple.mail DisableReplyAnimations -bool {0} && defaults write com.apple.mail DisableSendAnimations -bool {0}',
+            'type': 'boolean',
+        },
+        'AddressesIncludeNameOnPasteboard': {
+            'label': 'Copy email addresses as <code>foo@example.com</code> instead of <code>Foo Bar &lt;foo@example.com&gt;</code>.',
+            'command': 'defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool {0}',
+            'type': 'boolean',
+        },
+        'NSUserKeyEquivalents': {
+            'label': 'Add the keyboard shortcut ⌘ + Enter to send an email.',
+            'command': 'defaults write com.apple.mail NSUserKeyEquivalents -dict-add "Send" "@\\U21a9"',
+            'type': 'none',
+        },
+        'DraftsViewerAttributes': {
+            'label': 'Display emails in threaded mode, sorted by date (oldest at the top)',
+            'command': 'defaults write com.apple.mail DraftsViewerAttributes -dict-add "DisplayInThreadedMode" -string "yes" && defaults write com.apple.mail DraftsViewerAttributes -dict-add "SortedDescending" -string "yes" && defaults write com.apple.mail DraftsViewerAttributes -dict-add "SortOrder" -string "received-date"',
+            'type': 'none',
+        },
+        'DisableInlineAttachmentViewing': {
+            'label': 'Disable inline attachments (just show the icons).',
+            'command': 'defaults write com.apple.mail DisableInlineAttachmentViewing -bool {0}',
+            'type': 'boolean',
+        },
+        'Disable automatic spell checking': {
+            'label': 'Disable automatic spell checking.',
+            'command': 'defaults write com.apple.mail SpellCheckingBehavior -string "NoSpellCheckingEnabled"',
+            'type': 'none',
+        },
     },
     'Terminal & iTerm 2': {
         # '': {
@@ -987,31 +1012,6 @@ raw_command_config = {
     },
 }
 
-# ########################################/
-# # Mail                                                                        #
-# ########################################/
-#
-# # Disable send and reply animations in Mail.app
-# defaults write com.apple.mail DisableReplyAnimations -bool true
-# defaults write com.apple.mail DisableSendAnimations -bool true
-#
-# # Copy email addresses as `foo@example.com` instead of `Foo Bar <foo@example.com>` in Mail.app
-# defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
-#
-# # Add the keyboard shortcut ⌘ + Enter to send an email in Mail.app
-# defaults write com.apple.mail NSUserKeyEquivalents -dict-add "Send" "@\U21a9"
-#
-# # Display emails in threaded mode, sorted by date (oldest at the top)
-# defaults write com.apple.mail DraftsViewerAttributes -dict-add "DisplayInThreadedMode" -string "yes"
-# defaults write com.apple.mail DraftsViewerAttributes -dict-add "SortedDescending" -string "yes"
-# defaults write com.apple.mail DraftsViewerAttributes -dict-add "SortOrder" -string "received-date"
-#
-# # Disable inline attachments (just show the icons)
-# defaults write com.apple.mail DisableInlineAttachmentViewing -bool true
-#
-# # Disable automatic spell checking
-# defaults write com.apple.mail SpellCheckingBehavior -string "NoSpellCheckingEnabled"
-#
 # ########################################/
 # # Spotlight                                                                   #
 # ########################################/
